a677cfe7b00d1a3506baf921eed7a220ec53f0a9
Pedro Cezar <128489164+PedroCezardev@users.noreply.github.com>
2023-07-10 21:31:56 -0300

    Algoritmo usando Operadores de Atribuição
    Exemplo de Algoritmo que realiza cálculos usando Operadores de Atribuição, em Java usando lógica de programação

---
+++ Operadores.java
@@ -0,0 +1,24 @@
+// Algoritmo que realiza calculos usando Operadores de Atribuição, usando a linguagem java em lógica de programação.
+
+import java.util.Scanner;
+
+public class Operadores {
+    
+    public static void main(String[] args) {
+        int num = 0;
+        System.out.println("Valor original: " + num);
+        
+        num += 3;
+        System.out.println("valor de num: " + num);
+        
+        num *= 4;
+        System.out.println("Valor de num: " + num);
+        
+        num /= 2;
+        System.out.println("Valor de num: " + num);
+        
+        num %= 2;
+        System.out.println("Valor de num: " + num);
+        
+    }
+}
