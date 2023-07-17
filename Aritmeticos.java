d6727547d52f4cbbaa39f33c8d04e5889e31b248
Pedro Cezar <128489164+PedroCezardev@users.noreply.github.com>
2023-07-17 19:40:48 -0300

    Código de Operadores Aritméticos em java
    Código de exemplo de Operadores Aritméticos em java, usando lógica de programação.

---
+++ Aritmeticos.java
@@ -0,0 +1,20 @@
+// Exemplos de Operadores Aritméticos em java, guia para estudo, lógica de programação.
+
+
+import java.util.Scanner;
+
+public class Aritmeticos
+{
+	public static void main(String[] args) {
+		int x = 9;
+		int y = 2;
+		
+		System.out.printf("x + y = %d \n", x + y);
+		System.out.printf("x - y = %d \n", x - y);
+		System.out.printf("x * y = %d \n", x * y);
+		System.out.printf("x %% y = %d \n", x % y);
+		System.out.printf("x / y = %d \n", x / y); // Divisão inteira
+		System.out.printf("x / y = %2.2f \n", (double) x / y); // Divisão normal
+		
+	}
+}
