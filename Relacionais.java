469f60ddc7ea4cf46e868b8cb46168d6a463c8e5
Pedro Cezar <128489164+PedroCezardev@users.noreply.github.com>
2023-07-17 21:56:47 -0300

    Código de exemplo de Operadores Relacionais
    Códigos de exemplo no uso de Operadores Relacionais em Java, usando lógica de programação.

---
+++ Relacionais.java
@@ -0,0 +1,20 @@
+// Código de exemplo no uso de Operadores Relacionais em java, usando lógica de programação.
+
+import java.util.Scanner;
+
+public class Relacionais
+{
+	public static void main(String[] args) {
+		int x = 10;
+		int k = 20;
+		
+		System.out.printf("X é igual a 10? %b \n", x == 10);
+		System.out.printf("X é diferente de 10? %b \n", x != 10);
+		System.out.printf("X é Maior que k? %b \n", x > k);
+		System.out.printf("X é Maior ou Igual a 10? %b \n", x >= 10);
+		System.out.printf("X é Menor que 10? %b \n", x < 10);
+		System.out.printf("X é Menor ou Igual a k? %b \n", x <= k);
+		
+    // Operadores relacionais sempre retornam valores lógicos, true ou false.		
+	}
+}
